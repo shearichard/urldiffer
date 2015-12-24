563b0330c49703fb338cd773dbf23f78bdb17f86
Richard Shea <rshea@thecubagroup.com>
2015-12-24 16:57:08 +1300

    Improve output. Allow for category exclusions. Update README.md to reflect changes to .INI

--- README.md
+++ README.md
@@ -6,7 +6,13 @@ The .INI used by urldiffer.py looks like this :
     [baseline]
     baseurl = https://wiki.python.org/moin/ConfigParserExamples/
     [comparisons]
-    url1 = http://stackoverflow.com/questions/335695/lists-in-configparser
-    url2 = https://www.google.co.nz
-    url3 = http://www.theguardian.com/sport/2015/dec/23/champion-skier-marcel-hirscher-has-near-miss-as-drone-falls-out-of-sky
+    url1 = TFF|FIN|/query/?0_description=~&0_closed=0&0_owner=137&0_financial_colour=red&1_closed=0&1_data_entry_user=137&1_financial_colour=red&order=id&col=id&col=description&col=owner&col=data_entry_user&col=financial_colour&col=budget&col=actual&col=forecast&col=financial_variance&col=financial_explanation&as_of=as_of 
+    url2 = TFF|INHERENTRISK|/query/?0_description=~&0_closed=u0&0_owner=137&0_risk_colour=red&1_closed=u0&1_data_entry_user=137&1_risk_colour=red&order=id&include_underlying=1&col=id&col=description&col=owner&col=data_entry_user&col=risk_colour&col=risk_description&col=risk_recommendation&col=risk_residual_colour&as_of=as_of
+    url3 = TFF|MITIGATEDRISK|/query/?0_description=~&0_closed=u0&0_owner=137&0_risk_residual_colour=red&1_closed=u0&1_data_entry_user=137&1_risk_residual_colour=red&order=id&include_underlying=1&col=id&col=description&col=owner&col=data_entry_user&col=risk_colour&col=risk_description&col=risk_recommendation&col=risk_residual_colour&as_of=as_of
+    url4 =TTF|FIN|https://design.opal3.com/query/?0_description=~&0_closed=u0&0_owner=137&0_financial_colour=red&1_closed=u0&1_data_entry_user=137&1_financial_colour=red&order=id&include_underlying=1&col=id&col=description&col=owner&col=data_entry_user&col=financial_colour&col=budget&col=actual&col=forecast&col=financial_variance&col=financial_explanation&as_of=as_of
+    url5 =TTF|INHERENTRISK|https://design.opal3.com/query/?0_description=~&0_closed=u0&0_owner=137&0_risk_colour=red&1_closed=u0&1_data_entry_user=137&1_risk_colour=red&order=id&include_underlying=1&col=id&col=description&col=owner&col=data_entry_user&col=risk_colour&col=risk_description&col=risk_recommendation&col=risk_residual_colour&as_of=as_of
+    url6 =TTF|MITIGATEDRISK|https://design.opal3.com/query/?0_description=~&0_closed=u0&0_owner=137&0_risk_residual_colour=red&1_closed=u0&1_data_entry_user=137&1_risk_residual_colour=red&order=id&include_underlying=1&col=id&col=description&col=owner&col=data_entry_user&col=risk_colour&col=risk_description&col=risk_recommendation&col=risk_residual_colour&as_of=as_of
+    [excludecategories]
+    cat1=INHERENTRISK
+    cat2=MITIGATEDRISK
 
--- urldiffer/urldiffer.py
+++ urldiffer/urldiffer.py
@@ -26,13 +26,23 @@ def get_ini_data():
     if False:
         config_diag(parser)
       
+    cat_items = parser.items( "excludecategories" )
+    lst_ex_cat = []
+    for key, value in cat_items:
+        lst_ex_cat.append(value)
+
+
     path_items = parser.items( "comparisons" )
     dicout = {}
     for key, value in path_items:
         lst_val = value.split("|")
-        dicout[lst_val[0]] = lst_val[1]
+        category = lst_val[0]
+        if lst_val[0] not in dicout:
+            dicout[lst_val[0]] = {}
+        dicout[lst_val[0]][lst_val[1]] = lst_val[2]
+
     baselineurl = parser.get('baseline', 'baseurl') 
-    return {'baselineurl': baselineurl, 'diccompurls': dicout}
+    return {'excluded_categories': lst_ex_cat, 'baselineurl': baselineurl, 'diccompurls': dicout}
 
 def parse_query(url):
     '''
@@ -44,15 +54,24 @@ def parse_query(url):
     p_actual = parse_qs(o_actual.query)
     return p_actual
 
-def find_query_diff(baselineurl, dic_of_urls_to_compare):
+def find_query_diff(baselineurl, 
+                    dic_of_urls_to_compare,
+                    lst_excluded_categories):
     '''
     For each url in `lst_of_urls_to_compare` find those parts
     of the query which do not exist in the `baselineurl` and 
     report on it.
     '''
     dic_urls_qry = {}
-    for desc, url in dic_of_urls_to_compare.iteritems():
-        dic_urls_qry[desc] = parse_query(url)
+    for settings, urldic in dic_of_urls_to_compare.iteritems():
+        if settings not in dic_urls_qry:
+            dic_urls_qry[settings] = {}
+
+        for desc, url in urldic.iteritems():
+            if desc not in lst_excluded_categories:
+                if desc not in dic_urls_qry[settings]:
+                    dic_urls_qry[settings][desc] = {}
+                dic_urls_qry[settings][desc] = parse_query(url)
 
     baseline_urls_qry = parse_query(baselineurl)
 
@@ -61,33 +80,40 @@ def find_query_diff(baselineurl, dic_of_urls_to_compare):
     pprint.pprint(baseline_urls_qry)
 
     print("XXX")
-    for desc, dic_url_qry in dic_urls_qry.iteritems():
+    import pdb
+    pdb.set_trace()
+    for settings, dicurl in dic_urls_qry.iteritems():
         print()
-        lstout = []
-        for argname, argval in dic_url_qry.iteritems():
-            if argname in baseline_urls_qry:
-                if (collections.Counter(dic_url_qry[argname]) == collections.Counter(baseline_urls_qry[argname])):
+        print("- " + settings)
+        for desc, dic_url_qry in dicurl.iteritems():
+            print()
+            print("-- " + desc)
+            lstout = []
+            for argname, argval in dic_url_qry.iteritems():
+                if argname in baseline_urls_qry:
+                    if (collections.Counter(dic_url_qry[argname]) == collections.Counter(baseline_urls_qry[argname])):
+                        pass
+                    else:
+                        lstout.append(argname + " exists in both " + desc + " and baseline but values are different. Value in " + desc + " is " + str(dic_url_qry[argname]) + " . Value in baseline is " + str(baseline_urls_qry[argname]) + " .")
+                else:
+                    lstout.append(argname + " exists in " + desc + " but not in baseline")
+
+            for argname, argval in baseline_urls_qry.iteritems():
+                if argname in dic_url_qry:
                     pass
                 else:
-                    lstout.append(argname + " exists in both " + desc + " and baseline but values are different. Value in " + desc + " is " + str(dic_url_qry[argname]) + " . Value in baseline is " + str(baseline_urls_qry[argname]) + " .")
-            else:
-                lstout.append(argname + " exists in " + desc + " but not in baseline")
+                    lstout.append(argname + " exists in baseline but not in " + desc )
 
-        for argname, argval in baseline_urls_qry.iteritems():
-            if argname in dic_url_qry:
-                pass
-            else:
-                lstout.append(argname + " exists in baseline but not in " + desc )
-
-        lstout.sort()
-        for msg in lstout:
-            print(msg)
+            lstout.sort()
+            for msg in lstout:
+                print(msg)
 
 
 def main():
     dic_ini_data = get_ini_data()
     find_query_diff(dic_ini_data['baselineurl'],
-                    dic_ini_data['diccompurls'])
+                    dic_ini_data['diccompurls'],
+                    dic_ini_data['excluded_categories'])
 
 if __name__ == "__main__":
     main()
